b208570c9659aba9336a27eb1e116045c25ddb93
Anis <sadikowas@yandex.ru>
2023-12-30 08:35:23 +0300

    lab6

--- t1.c
+++ t1.c
@@ -1,6 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 
-int main(int argc, char *argv[]){
-	printf("Hello world!\n");
-	return 0;
-};
+#include <time.h>
+#include <sys/time.h>
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+void printTime() {
+ 	struct timeval tv;
+	gettimeofday(&tv, NULL); 
+	
+	struct tm *tm_info;
+    	tm_info = localtime(&tv.tv_sec);
+
+    	printf("Time: %02d:%02d:%02d:%03ld\n", tm_info->tm_hour, tm_info->tm_min, tm_info->tm_sec, tv.tv_usec / 1000);
+}
+
+int main() {
+	
+	pid_t child1 = fork();
+	
+	if (child1 == -1) {
+        	perror("Child 1 fork error!");
+        	exit(errno);
+    	} else if (child1 == 0) {
+        	printf("Child 1: pid=%d, ppid=%d, ", getpid(), getppid());
+        	printTime();
+        	exit(EXIT_SUCCESS);
+    	}
+    	
+    	pid_t child2 = fork();
+
+    	if (child2 == -1) {
+        	perror("Child 2 fork error!");
+        	exit(errno);
+    	} else if (child2 == 0) {
+        	printf("Child 2: pid=%d, ppid=%d, ", getpid(), getppid());
+        	printTime();
+        	exit(EXIT_SUCCESS);
+    	}
+
+	printf("Parent: pid=%d, ppid=%d, ", getpid(), getppid());
+    	printTime();
+    	
+
+	waitpid(child1, NULL, 0);
+    	waitpid(child2, NULL, 0);
+
+	if (system("ps -x | grep \"t1\"") != 0) {
+        	perror("System call error!");
+        	exit(errno);
+    	}
+
+    	
+    	return 0;
+}
+
--- t2.c
+++ t2.c
@@ -1,34 +1,150 @@
-#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-int main(int argc, char *argv[]){
-	if (argc != 2){
-		printf("Enter the file name!\n");
-		return 1;
-	};
+char* concat(const char *s1, const char *s2)
+{
+    char *result = malloc(strlen(s1) + strlen(s2) + 1); 
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
+int copyFile(const char *src_path, const char *dst_path) {
+	int total_bytes = 0;
 	
+	FILE *src = fopen(src_path, "r");
+	if (src == NULL) {
+		perror(src_path);
+		exit(errno);
+	}
+
+	FILE *dst = fopen(dst_path, "w");
+	if (dst == NULL) {
+		perror(dst_path);
+		exit(errno);
+	}
 	
-	FILE *f = fopen(argv[1], "w");
-	if (f == NULL){
-		perror("fopen failed!");
-		return errno;
-	};
-	
-	int c = '\0';
-	while (c != EOF && c != 0x06) {
-		c = fgetc(stdin); 
-		if (ferror(stdin)) { 
+	char c = '\0';
+	while (1)  {
+		c = fgetc(src);
+		if (ferror(src)) { 
         		perror("File read error!\n"); 
-        		return errno;
-        	};
-		
-		fputc(c, f);
-		if (ferror(f)) { 
+        		exit(errno);
+        	}
+        	  
+		if (c == EOF) break;
+		fputc(c, dst);
+		if (ferror(src)) { 
         		perror("File write error!\n"); 
-        		return errno;
-        	};
-	};
+        		exit(errno);
+        	}
+	}
+
+	fclose(src);
+	fclose(dst);
 	
-	fclose(f);
+	struct stat src_stat;
+	stat(src_path, &src_stat);
+	chmod(dst_path, src_stat.st_mode);
+	
+	return src_stat.st_size;
+}
+
+
+int main(int argc, char *argv[]) {
+	if (argc != 4){
+		printf("Usage: <source directory> <destination directory> <max processes>\n");
+		return 1;
+	}
+
+	DIR *src_dir = opendir(argv[1]);
+	if (src_dir == NULL){
+		perror("Source directory open failed!");
+		exit(errno);
+	}
+	
+	
+	int max_proc = atoi(argv[3]);
+	
+	int runningProcesses = 0;
+	struct dirent *d;
+
+	while ((d = readdir(src_dir)) != NULL){
+		if (d->d_type != DT_DIR){
+		
+			if (runningProcesses >= max_proc) {
+			    wait(NULL);
+			    runningProcesses--;
+			}
+			
+			int pid = fork();
+			if (pid == -1) {
+			    perror("Fork_error!");
+			    exit(EXIT_FAILURE);
+			} else if (pid == 0) { 
+			    printf("PID: %d, Start copying file: %s\n", getpid(), d->d_name);
+			    
+			    int bytes_copyed = copyFile(concat(argv[1], concat("/", d->d_name)), concat(argv[2], concat("/", d->d_name)));
+			    
+			    printf("PID: %d, Copying is completed, %d bytes have been copied\n", getpid(), bytes_copyed);
+			   
+			    exit(EXIT_SUCCESS);
+			}
+
+			runningProcesses++;
+			
+		}
+	}
+	
+	
+	
+	if (closedir(src_dir) == -1) {
+    		perror("Source directory close failed!");
+    		exit(errno);
+	}
+	
+	
+	if (system("ps -x | grep \"t2\" | grep -v \"grep\"") != 0) {
+        	perror("System call error!");
+        	exit(errno);
+    	}
+	while (wait(NULL) > 0);
+	
 	return 0;
-};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
